cfad8da8973641bef7931a680f4bd5e32abde658
wandl <hnxxyhcwdl1003@163.com>
2022-07-25 22:36:26 +0800

    2.0.0-SNAPSHOT

--- src/main/java/org/springframework/security/boot/SecurityFacebookFilterConfiguration.java
+++ src/main/java/org/springframework/security/boot/SecurityFacebookFilterConfiguration.java
@@ -15,14 +15,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.boot.biz.authentication.AuthenticationListener;
 import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationEntryPoint;
 import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationFailureHandler;
 import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationSuccessHandler;
-import org.springframework.security.boot.biz.property.SecuritySessionMgtProperties;
 import org.springframework.security.boot.facebook.authentication.FacebookAccessTokenAuthenticationProcessingFilter;
-import org.springframework.security.boot.utils.WebSecurityUtils;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -38,14 +37,14 @@ import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
 
 @Configuration
-@AutoConfigureBefore(name = { 
+@AutoConfigureBefore(name = {
 	"org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration"
 })
 @ConditionalOnWebApplication
 @ConditionalOnProperty(prefix = SecurityFacebookProperties.PREFIX, value = "enabled", havingValue = "true")
 @EnableConfigurationProperties({ SecurityFacebookProperties.class, SecurityFacebookAuthcProperties.class, SecurityBizProperties.class, ServerProperties.class })
 public class SecurityFacebookFilterConfiguration {
-	
+
 	@Configuration
 	@EnableConfigurationProperties({ SecurityFacebookProperties.class, SecurityFacebookAuthcProperties.class, SecurityBizProperties.class })
 	@Order(SecurityProperties.DEFAULT_FILTER_ORDER + 6)
@@ -61,16 +60,16 @@ public class SecurityFacebookFilterConfiguration {
     	private final RememberMeServices rememberMeServices;
 		private final SessionAuthenticationStrategy sessionAuthenticationStrategy;
 		private final OkHttpClient okhttp3Client;
-		
+
 		public FacebookWebSecurityConfigurerAdapter(
-   				
+
 				SecurityBizProperties bizProperties,
-				SecuritySessionMgtProperties sessionMgtProperties,
 				SecurityFacebookAuthcProperties authcProperties,
-				
+
 				ObjectProvider<LocaleContextFilter> localeContextProvider,
 				ObjectProvider<AuthenticationProvider> authenticationProvider,
    				ObjectProvider<AuthenticationListener> authenticationListenerProvider,
+				ObjectProvider<AuthenticationManager> authenticationManagerProvider,
    				ObjectProvider<MatchedAuthenticationEntryPoint> authenticationEntryPointProvider,
    				ObjectProvider<MatchedAuthenticationSuccessHandler> authenticationSuccessHandlerProvider,
    				ObjectProvider<MatchedAuthenticationFailureHandler> authenticationFailureHandlerProvider,
@@ -79,22 +78,22 @@ public class SecurityFacebookFilterConfiguration {
    				ObjectProvider<RememberMeServices> rememberMeServicesProvider,
    				ObjectProvider<SessionAuthenticationStrategy> sessionAuthenticationStrategyProvider
 				) {
-			
-			super(bizProperties, sessionMgtProperties, authenticationProvider.stream().collect(Collectors.toList()));
-   			
+
+			super(bizProperties, authcProperties, authenticationProvider.stream().collect(Collectors.toList()), authenticationManagerProvider.getIfAvailable());
+
 			this.authcProperties = authcProperties;
-			
+
 			this.localeContextFilter = localeContextProvider.getIfAvailable();
    			List<AuthenticationListener> authenticationListeners = authenticationListenerProvider.stream().collect(Collectors.toList());
-   			this.authenticationEntryPoint = WebSecurityUtils.authenticationEntryPoint(authcProperties, sessionMgtProperties, authenticationEntryPointProvider.stream().collect(Collectors.toList()));
-   			this.authenticationSuccessHandler = WebSecurityUtils.authenticationSuccessHandler(authcProperties, sessionMgtProperties, authenticationListeners, authenticationSuccessHandlerProvider.stream().collect(Collectors.toList()));
-   			this.authenticationFailureHandler = WebSecurityUtils.authenticationFailureHandler(authcProperties, sessionMgtProperties, authenticationListeners, authenticationFailureHandlerProvider.stream().collect(Collectors.toList()));
-   			this.objectMapper = objectMapperProvider.getIfAvailable();
+			this.authenticationEntryPoint = super.authenticationEntryPoint(authenticationEntryPointProvider.stream().collect(Collectors.toList()));
+			this.authenticationSuccessHandler = super.authenticationSuccessHandler(authenticationListeners, authenticationSuccessHandlerProvider.stream().collect(Collectors.toList()));
+			this.authenticationFailureHandler = super.authenticationFailureHandler(authenticationListeners, authenticationFailureHandlerProvider.stream().collect(Collectors.toList()));
+			this.objectMapper = objectMapperProvider.getIfAvailable();
    			this.rememberMeServices = rememberMeServicesProvider.getIfAvailable();
    			this.sessionAuthenticationStrategy = sessionAuthenticationStrategyProvider.getIfAvailable();
-   			
-   			this.okhttp3Client = okhttp3ClientProvider.getIfAvailable(() -> { 
-   				
+
+   			this.okhttp3Client = okhttp3ClientProvider.getIfAvailable(() -> {
+
    				OkHttpClient.Builder builder = new OkHttpClient.Builder();
    		        builder.connectTimeout(6L, TimeUnit.SECONDS);
    		        builder.readTimeout(6L, TimeUnit.SECONDS);
@@ -102,41 +101,41 @@ public class SecurityFacebookFilterConfiguration {
    		        ConnectionPool connectionPool = new ConnectionPool(50, 60, TimeUnit.SECONDS);
    		        builder.connectionPool(connectionPool);
    		        return builder.build();
-   		        
+
    			});
 		}
 
-		
+
 		public FacebookAccessTokenAuthenticationProcessingFilter authenticationProcessingFilter() throws Exception {
-	    	
+
 			FacebookAccessTokenAuthenticationProcessingFilter authenticationFilter = new FacebookAccessTokenAuthenticationProcessingFilter(this.objectMapper, this.okhttp3Client);
-			
+
 			/**
 			 * 批量设置参数
 			 */
 			PropertyMapper map = PropertyMapper.get().alwaysApplyingWhenNonNull();
-			
-			map.from(getSessionMgtProperties().isAllowSessionCreation()).to(authenticationFilter::setAllowSessionCreation);
-			
+
+			map.from(authcProperties.getSessionMgt().isAllowSessionCreation()).to(authenticationFilter::setAllowSessionCreation);
+
 			map.from(authenticationManagerBean()).to(authenticationFilter::setAuthenticationManager);
 			map.from(authenticationSuccessHandler).to(authenticationFilter::setAuthenticationSuccessHandler);
 			map.from(authenticationFailureHandler).to(authenticationFilter::setAuthenticationFailureHandler);
-			
+
 			map.from(authcProperties.getAppSecret()).to(authenticationFilter::setAppSecret);
 			map.from(authcProperties.getFields()).to(authenticationFilter::setFields);
 			map.from(authcProperties.getAlgorithm()).to(authenticationFilter::setAlgorithm);
-			
+
 			map.from(authcProperties.getPathPattern()).to(authenticationFilter::setFilterProcessesUrl);
 			map.from(rememberMeServices).to(authenticationFilter::setRememberMeServices);
 			map.from(sessionAuthenticationStrategy).to(authenticationFilter::setSessionAuthenticationStrategy);
 			map.from(authcProperties.isContinueChainBeforeSuccessfulAuthentication()).to(authenticationFilter::setContinueChainBeforeSuccessfulAuthentication);
-			
+
 	        return authenticationFilter;
 	    }
-		
+
 		@Override
 		public void configure(HttpSecurity http) throws Exception {
-			
+
 			http.antMatcher(authcProperties.getPathPattern())
 				.exceptionHandling()
 	        	.authenticationEntryPoint(authenticationEntryPoint)
@@ -144,19 +143,19 @@ public class SecurityFacebookFilterConfiguration {
 	        	.httpBasic()
 	        	.disable()
 	        	.addFilterBefore(localeContextFilter, UsernamePasswordAuthenticationFilter.class)
-   	        	.addFilterBefore(authenticationProcessingFilter(), UsernamePasswordAuthenticationFilter.class); 
-   	    	
+   	        	.addFilterBefore(authenticationProcessingFilter(), UsernamePasswordAuthenticationFilter.class);
+
    	    	super.configure(http, authcProperties.getCors());
    	    	super.configure(http, authcProperties.getCsrf());
    	    	super.configure(http, authcProperties.getHeaders());
 	    	super.configure(http);
 		}
-		
+
 		@Override
 	    public void configure(WebSecurity web) throws Exception {
 	    	super.configure(web);
 	    }
-		
+
 	}
-	
+
 }
