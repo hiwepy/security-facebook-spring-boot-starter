ee70fe0c0022b1b92b5a6db513b0d671b72d5a4c
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:58:53 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.0.x branch

--- src/main/java/org/springframework/security/boot/facebook/exception/FacebookAccessTokenExpiredException.java
+++ src/main/java/org/springframework/security/boot/facebook/exception/FacebookAccessTokenExpiredException.java
@@ -19,7 +19,7 @@ import org.springframework.security.boot.biz.exception.AuthenticationTokenExpire
 
 /**
  * Facebook AccessToken Expired Exception
- * @author 		： <a href="https://github.com/hiwepy">wandl</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class FacebookAccessTokenExpiredException extends AuthenticationTokenExpiredException {
 	
