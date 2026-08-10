7ed0bc1fc77a8d94db6e8b1fed10983c0da6b515
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:55:40 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/facebook/exception/FacebookAccessTokenInvalidException.java
+++ src/main/java/org/springframework/security/boot/facebook/exception/FacebookAccessTokenInvalidException.java
@@ -19,7 +19,7 @@ import org.springframework.security.boot.biz.exception.AuthenticationTokenInvali
 
 /**
  * Facebook AccessToken Invalid Exception
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class FacebookAccessTokenInvalidException extends AuthenticationTokenInvalidException {
 	
