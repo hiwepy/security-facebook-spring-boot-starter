cba724ff97c8de64c71bdf31bf1fc0c976e7c515
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:55:48 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/facebook/exception/FacebookAccessTokenExpiredException.java
+++ src/main/java/org/springframework/security/boot/facebook/exception/FacebookAccessTokenExpiredException.java
@@ -19,7 +19,7 @@ import org.springframework.security.boot.biz.exception.AuthenticationTokenExpire
 
 /**
  * Facebook AccessToken Expired Exception
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class FacebookAccessTokenExpiredException extends AuthenticationTokenExpiredException {
 	
